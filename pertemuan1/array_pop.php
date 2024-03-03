15a9bbab16e2a0d85a465feb7074b4712a4b279c
Marcrisca <146653561+Marcrisca@users.noreply.github.com>
2024-03-03 15:28:49 +0700

    Add files via upload

---
+++ pertemuan1/array_pop.php
@@ -0,0 +1,8 @@
+<?php
+$tims = ["erwin", "heru", "ali", "zaki"];
+
+array_pop($tims); //menghapus elemen terakhir didalam array
+foreach ($tims as $person) {
+    echo $person . "</br>";
+}
+
---
+++ pertemuan1/variable_user.php
@@ -0,0 +1,10 @@
+<?php
+$nama = "Risca Marcella Jhesica";
+$umur = 19;
+$berat = 77;
+
+echo 'Nama :' . $nama;
+echo '</br>Umur :' . $umur . 'Tahun';
+echo '</br>Berat :' .$berat . 'Kg';
+
+echo "</br>Hello $nama Apakabar";
