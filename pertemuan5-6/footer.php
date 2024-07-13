e1a891449e9a47ea6d3b17f6940fc8dd6a3bdf14
Marcrisca <146653561+Marcrisca@users.noreply.github.com>
2024-07-13 10:45:27 +0700

    footer.php

--- pertemuan5-6/footer.php
+++ pertemuan5-6/footer.php
@@ -4,7 +4,7 @@
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.2.0
     </div>
-    <strong>Copyright &copy; 2024 <a href="#">Febryan Ahmad Tri Setyo</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2024 <a href="#">Risca Marcella Jhesica</a>.</strong> All rights reserved.
   </footer>
 
   <!-- Control Sidebar -->
